27f6c2f463f04bba32819e6cc426ca87ce3a018e
Adrian Ang <adriancbang@gmail.com>
2021-02-15 18:33:27 -0800

    Initial commit

---
+++ part2/phonebook/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import App from './App';
+
+const App = () => {
+  return (
+    <div>
+      Hello World
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
