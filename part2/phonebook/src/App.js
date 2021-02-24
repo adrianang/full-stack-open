886d951f156e54c1db0a37de0a933431f264ba93
Adrian Ang <adriancbang@gmail.com>
2021-02-23 21:59:39 -0800

    Complete exercise 2.19: phonebook, step 11 (show notification after a successful operation is executed)

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import Notification from './components/Notification'
 import noteService from './services/notes'
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
   const [newName, setName] = useState('')
   const [newNumber, setNumber] = useState('')
   const [filter, setFilter] = useState('')
+  const [message, setMessage] = useState(null)
 
   const hook = () => {
     noteService
@@ -27,19 +29,23 @@ const App = () => {
       number: newNumber
     }
     const checkPersonExists = persons.some(person => person.name === newName)
-    const idToUpdate = persons.find(person => person.name === newName).id
+    
     const resetNewContactInputs = () => {
       setName('')
       setNumber('')
     }
 
     if (checkPersonExists) {
+      const idToUpdate = persons.find(person => person.name === newName).id
+
       if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
         return noteService
           .update(idToUpdate, personObject)
           .then(returnedPerson => {
             setPersons(persons.map(person => person.id === idToUpdate ? returnedPerson : person))
             resetNewContactInputs()
+            setMessage(`Updated ${newName}'s number`)
+            setTimeout(() => setMessage(null), 5000)
           })
       }
       return
@@ -50,6 +56,8 @@ const App = () => {
       .then(returnedPerson => {
         setPersons(persons.concat(returnedPerson))
         resetNewContactInputs()
+        setMessage(`Added ${newName} to phonebook`)
+        setTimeout(() => setMessage(null), 5000)
       })
   }
 
@@ -58,7 +66,11 @@ const App = () => {
     if (window.confirm(`Delete ${nameToDelete}?`)) {
       noteService
         .remove(id)
-        .then(setPersons(persons.filter(person => person.id !== id)))
+        .then(returnedPerson => {
+          setPersons(persons.filter(person => person.id !== id))
+          setMessage(`Deleted ${nameToDelete} from phonebook`)
+          setTimeout(() => setMessage(null), 5000)
+        })
     }
   }
 
@@ -72,6 +84,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={message} />
       <Filter filter={filter} handleFilterChange={handleFilterChange} />
 
       <h2>Add a New Contact</h2>
